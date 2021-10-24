11d05b49e8e854d838e9f8fa21331b4331535499
Lewis Nozaki <lhnozaki@outlook.com>
2021-10-23 16:37:50 -1000

    updates

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,6 +1,12 @@
+// Import the User model
 const User = require("../models/user.models");
 
-///////////
+// Error handling and extracting from MongoDB validation
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+}
+
+////////////
 // Signup //
 ////////////
 
@@ -16,6 +22,8 @@ const signup_post = async (req, res) => {
 
     res.status(201).json(user);
   } catch (err) {
+    handleErrors(err);
+
     res.status(400).send("Error: ", err);
   }
 };
--- models/user.models.js
+++ models/user.models.js
@@ -1,18 +1,21 @@
 // Import Mongoose
 const { Schema, model } = require("mongoose");
 
+// Import the validator package
+const { isEmail } = require("validator");
+
 const emailReq = {
   type: String,
   required: [true, "Please enter an email."],
   unique: true,
   lowercase: true,
-  validate: [(val) => {  }, "Please enter a valid email."]
+  validate: [isEmail, "Please enter a valid email."]
 };
 
 const passwordReq = {
   type: String,
   required: [true, "Please enter a password."],
-  minLength: [6, "The minimum password length is 6 characters."]
+  minlength: [6, "The minimum password length is 6 characters."]
 };
 
 const userSchema = new Schema({
